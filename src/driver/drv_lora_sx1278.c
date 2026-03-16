27315db0579ad152d6ccf455f376acc83f7c58f9
vash-sa <78752348+vash-sa@users.noreply.github.com>
2026-03-16 17:14:32 +0300

    Update drv_lora_sx1278.c

--- src/driver/drv_lora_sx1278.c
+++ src/driver/drv_lora_sx1278.c
@@ -8,13 +8,12 @@
 #include "drv_public.h"
 #include "drv_local.h"
 #include "../hal/hal_pins.h"
-#include "../mqtt/new_mqtt.h"
 
 #include <string.h>
-#include <stdio.h>
 #include "driver/spi_master.h"
 #include "esp_log.h"
 
+// Глобальная переменная для SPI
 static spi_device_handle_t lora_spi;
 
 #define LORA_NSS  7
@@ -22,14 +21,14 @@ static spi_device_handle_t lora_spi;
 #define LORA_DIO0 3
 
 static const uint8_t ENCRYPT_KEY[16] = {'1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F'};
-static uint32_t g_registered_nodes = 0; 
 
 void LoRa_WriteReg(uint8_t addr, uint8_t val) {
     uint8_t data[2] = { (uint8_t)(addr | 0x80), val };
     spi_transaction_t t;
     memset(&t, 0, sizeof(t));
     t.length = 16;
-    t.tx_buffer = data;    
+    t.tx_buffer = data;
+    
     HAL_PIN_SetOutputValue(LORA_NSS, 0);
     spi_device_polling_transmit(lora_spi, &t);
     HAL_PIN_SetOutputValue(LORA_NSS, 1);
@@ -43,44 +42,13 @@ uint8_t LoRa_ReadReg(uint8_t addr) {
     t.length = 8;
     t.tx_buffer = &cmd;
     t.rxlength = 8;
-    t.rx_buffer = &res;    
+    t.rx_buffer = &res;
+    
     HAL_PIN_SetOutputValue(LORA_NSS, 0);
     spi_device_polling_transmit(lora_spi, &t);
     HAL_PIN_SetOutputValue(LORA_NSS, 1);
     return res;
 }
-
-
-// РЕГИСТРАЦИЯ УСТРОЙСТВА В HA
-void LoRa_SendDiscovery(int id) {
-    char t[64];
-    char p[256];
-
-    // 1. Температура (3 штуки %d в строке -> 3 аргумента id)
-    snprintf(t, sizeof(t), "homeassistant/sensor/lora_%d_t/config", id);
-    snprintf(p, sizeof(p), "{\"name\":\"Temp\",\"stat_t\":\"lora/%d/s\",\"val_tpl\":\"{{value_json.t}}\",\"unit_of_meas\":\"°C\",\"dev_cla\":\"temperature\",\"dev\":{\"ids\":[\"lora_%d\"],\"name\":\"LoRa Node %d\"}}", 
-            id, id, id);
-    MQTT_Publish(t, p, 0, 1);
-
-    // 2. Батарея (2 штуки %d в строке -> 2 аргумента id)
-    snprintf(t, sizeof(t), "homeassistant/sensor/lora_%d_v/config", id);
-    snprintf(p, sizeof(p), "{\"name\":\"Batt\",\"stat_t\":\"lora/%d/s\",\"val_tpl\":\"{{value_json.v}}\",\"unit_of_meas\":\"V\",\"dev_cla\":\"voltage\",\"dev\":{\"ids\":[\"lora_%d\"]}}", 
-            id, id);
-    MQTT_Publish(t, p, 0, 1);
-
-    // 3. Газ (2 штуки %d в строке -> 2 аргумента id)
-    snprintf(t, sizeof(t), "homeassistant/sensor/lora_%d_p/config", id);
-    snprintf(p, sizeof(p), "{\"name\":\"Gas\",\"stat_t\":\"lora/%d/s\",\"val_tpl\":\"{{value_json.p}}\",\"unit_of_meas\":\"ppm\",\"dev\":{\"ids\":[\"lora_%d\"]}}", 
-            id, id);
-    MQTT_Publish(t, p, 0, 1);
-
-    // 4. Дым (2 штуки %d в строке -> 2 аргумента id)
-    snprintf(t, sizeof(t), "homeassistant/binary_sensor/lora_%d_s/config", id);
-    snprintf(p, sizeof(p), "{\"name\":\"Smoke\",\"stat_t\":\"lora/%d/s\",\"val_tpl\":\"{{'ON' if value_json.s=='YES' else 'OFF'}}\",\"dev_cla\":\"smoke\",\"dev\":{\"ids\":[\"lora_%d\"]}}", 
-            id, id);
-    MQTT_Publish(t, p, 0, 1);
-}
-
 
 void LoRa_Init_Driver() {
     spi_bus_config_t buscfg = {
@@ -122,9 +90,8 @@ void LoRa_Init_Driver() {
     addLogAdv(LOG_INFO, LOG_FEATURE_DRV, "LoRa SX1278 Initialized");
 }
 
-
 void LoRa_RunFrame() {
-       if (HAL_PIN_ReadDigitalInput(LORA_DIO0)) {
+    if (HAL_PIN_ReadDigitalInput(LORA_DIO0)) {
         uint8_t irq = LoRa_ReadReg(0x12);
         LoRa_WriteReg(0x12, irq);
 
@@ -150,23 +117,15 @@ void LoRa_RunFrame() {
 
             float vcc, temp;
             int ppm;
-            char smoke[16];
+            char smoke[8];
             if (sscanf((char*)&buffer[2], "%f,%f,%[^,],%d", &vcc, &temp, smoke, &ppm) == 4) {
-                // Регистрируем, если еще не было такого ID
-                if (id < 32 && !(g_registered_nodes & (1 << id))) {
-                    LoRa_SendDiscovery(id);
-                    g_registered_nodes |= (1 << id);
-                }
-
-                // Шлем данные состояния в JSON
-                char s_topic[64], s_payload[256];
-                snprintf(s_topic, sizeof(s_topic), "lora/%d/s", id);
-                snprintf(s_payload, sizeof(s_payload), "{\"v\":%.2f,\"t\":%.1f,\"s\":\"%s\",\"p\":%d}", vcc, temp, smoke, ppm);
-                MQTT_Publish(s_topic, s_payload, 0, 0);
-
-                addLogAdv(LOG_INFO, LOG_FEATURE_DRV, "Node %d: T=%.1f V=%.2f Smoke=%s", id, temp, vcc, smoke);
+                CHANNEL_Set(1, (int)(vcc * 10), 0);
+                CHANNEL_Set(2, (int)temp, 0);
+                CHANNEL_Set(3, (strcmp(smoke, "YES") == 0 ? 1 : 0), 0);
+                CHANNEL_Set(4, ppm, 0);
             }
         }
     }
 }
+
 #endif
