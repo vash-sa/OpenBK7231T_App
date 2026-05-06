d5cc88174e5aaae549be4df50eeced45457741b5
vash-sa <78752348+vash-sa@users.noreply.github.com>
2026-05-06 12:47:40 +0300

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -426,10 +426,10 @@
 //#define ENABLE_DRIVER_IRREMOTEESP				1
 //#define ENABLE_DRIVER_TUYAMCU					1
 //#define ENABLE_DRIVER_TINYIR_NEC				1
-#define ENABLE_DRIVER_AHT2X						  1
-#define ENABLE_I2C							        1
-//#define ENABLE_DRIVER_BMPI2C					  1
-#define ENABLE_DRIVER_BMP280             1
+//#define ENABLE_DRIVER_AHT2X						  1
+//#define ENABLE_I2C							        1
+#define ENABLE_DRIVER_BMPI2C					  1
+//#define ENABLE_DRIVER_BMP280             1
 
 #elif PLATFORM_ESPIDF
 
